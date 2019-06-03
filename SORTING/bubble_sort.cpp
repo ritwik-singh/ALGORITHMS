aca6903e447c77fa880a9e6c21aa9f60d9e6cdf0
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-06-03 13:03:04 +0530

    updated bubble_sort.cpp

--- SORTING/bubble_sort.cpp
+++ SORTING/bubble_sort.cpp
@@ -1,1 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void bubble_s(int n, int a[])
+{
+	int temp;
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+				
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	bubble_s(n,a);
+	for(int i=0;i<n;i++)
+		cout<<a[i]<<" ";
+	return 0;
+}
