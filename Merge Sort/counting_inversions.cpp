4d76f00d628db55d3231054b302441c2fc78c6ce
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-06-13 15:34:19 +0530

    Create counting_inversions.cpp

---
+++ Merge Sort/counting_inversions.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int count = 0;
+
+void merge(long long int start,long long int end,long long  int mid,long long  int a[])
+{
+	long long int p = start;
+	long long int q = mid+1;
+	long long int copy[end-start+1];
+	long long int k=0;
+	for(long long int i=start;i<=end;i++)
+	{
+		if(p>mid)
+			copy[k++] = a[q++];
+		else if(q>end)
+			copy[k++] = a[p++];
+		else if(a[p] <=a[q])
+			copy[k++] = a[p++];
+		else
+			{
+			    ::count = ::count+(mid-p+1);
+			    copy[k++] = a[q++];
+			    
+			}		 
+
+	}
+	for(long long int i=0;i<k;i++)
+		a[start++] = copy[i];
+}
+
+void merge_sort(long long int start,long long int end,long long int a[])
+{
+	if(start<end){
+		long long int mid = (start+end)/2;
+		merge_sort(start,mid,a);
+		merge_sort(mid+1,end,a);
+		merge(start,end,mid,a);
+	}
+
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	long long int n;
+	cin>>n;
+	long long int a[n];
+	for(long long int i=0;i<n;i++)
+		cin>>a[i];
+	merge_sort(0,n-1,a);
+    cout<<::count<<endl;
+
+	return 0;
+}
