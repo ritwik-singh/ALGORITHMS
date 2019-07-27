734e488993f778f964e580a415c134487c9a491f
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-07-27 19:04:32 +0530

    Update activity_selection.cpp

--- Greedy Method/activity_selection.cpp
+++ Greedy Method/activity_selection.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printActivities(int start [],int finish [],int n)
+void printActivities(vector< pair <int,int> > vect,int n)
 {
-	
+	sort(vect.begin(), vect.end());
+	int i=0,j=0;
+	cout<<"("<<vect[0].second<<","<<vect[0].first<<") ";
+	int prev = vect[0].first;
+	for(int i=1;i<n;i++)
+	{
+		if(vect[i].second >= prev)
+		{
+			prev = vect[i].first;
+			cout<<"("<<vect[i].second<<","<<vect[i].first<<") ";
+		}
+	}
 }
 
 int main()
@@ -15,13 +26,17 @@ int main()
 
 	int n;
 	cin>>n;
+	vector< pair <int,int> > activity;
 	int start[n],finish[n];
 	for(int i=0;i<n;i++)
 		cin>>start[i];
 	for(int i=0;i<n;i++)
+	{
 		cin>>finish[i];
+		activity.push_back( make_pair(finish[i],start[i]) );
+	}	
 
-	printActivities(start,finish,n);
+	printActivities(activity,n);
 
 	return 0;
 }
