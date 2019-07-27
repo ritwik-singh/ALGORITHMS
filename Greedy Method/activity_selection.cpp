20f9516b0304ab02e428aed9592e38d19d215cd8
ritwik-singh <31265500+ritwik-singh@users.noreply.github.com>
2019-07-27 16:00:40 +0530

    Update activity_selection.cpp

--- Greedy Method/activity_selection.cpp
+++ Greedy Method/activity_selection.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void printActivities(int start [],int finish [],int n)
+{
+	
+}
+
+int main()
+{
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin>>n;
+	int start[n],finish[n];
+	for(int i=0;i<n;i++)
+		cin>>start[i];
+	for(int i=0;i<n;i++)
+		cin>>finish[i];
+
+	printActivities(start,finish,n);
+
+	return 0;
+}
